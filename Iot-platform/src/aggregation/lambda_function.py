dbbb4c3753defcdf619a5fbf568e41430566b92c
dasabinash <abinash.das08@gmail.com>
2026-04-20 07:38:16 +0530

    New line added

--- Iot-platform/src/aggregation/lambda_function.py
+++ Iot-platform/src/aggregation/lambda_function.py
@@ -2,6 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    print("A new line added")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
